f3ce9b84cdce1c2c06ea71a32fd13510e231154d
denis_babkin <babkin.denis1991@gmail.com>
2020-09-07 01:29:17 +0300

    Add README file

---
+++ my_second_script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Please enter two numbers ..."
+read a
+read b
+summa=$(($a+$b))
+echo $a + $b = $summa
---
+++ my_second_script_summa.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Enter two numbers ..."
+read number1
+read number2
+summa=$(($number1+$number2))
+echo "$number1 + $number2 = $summa"
+
